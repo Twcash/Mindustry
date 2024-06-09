d397cf88e200ae503b983e9c582e75bac0f25246
Twcash <131608563+Twcash@users.noreply.github.com>
2024-06-08 21:17:01 -0500

    Update Blocks.java

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -5438,7 +5438,7 @@ public class Blocks{
         }};
 
         exponentialReconstructor = new Reconstructor("exponential-reconstructor"){{
-            requirements(Category.units, with(Items.lead, 2000, Items.silicon, 1000, Items.titanium, 2000, Items.thorium, 750, Items.plastanium, 200));
+            requirements(Category.units, with(Items.lead, 1500, Items.silicon, 750, Items.titanium, 1000, Items.thorium, 750, Items.plastanium, 150));
 
             size = 7;
             consumePower(10f);
@@ -5460,7 +5460,7 @@ public class Blocks{
         }};
 
         tetrativeReconstructor = new Reconstructor("tetrative-reconstructor"){{
-            requirements(Category.units, with(Items.lead, 4000, Items.silicon, 3000, Items.thorium, 1000, Items.plastanium, 600, Items.phaseFabric, 600, Items.surgeAlloy, 800));
+            requirements(Category.units, with(Items.lead, 3000, Items.silicon, 2500, Items.thorium, 1250, Items.plastanium, 650, Items.phaseFabric, 500, Items.surgeAlloy, 500));
 
             size = 9;
             consumePower(15f);
